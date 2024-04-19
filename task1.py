395a0472aa48ac2e9578b1ec7e4ac056fd5918c1
Serhii Zhukov <sazhukov@gmail.com>
2024-04-19 19:42:18 +0300

    task1+try

--- task1.py
+++ task1.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    date_obj = datetime.strptime(date, '%Y-%m-%d')
+    try:
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        return "Wrong format of data. Use format 'YYYY-MM-DD'."
+    
     today = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
     delta = today - date_obj
-    return delta.days  
+    return delta.days
